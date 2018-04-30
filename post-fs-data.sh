c267c38aa3e04e0a7658b919105ca93b4ebb7c62
osm0sis <osm0sis@outlook.com>
2018-04-30 15:10:34 -0300

    fix boot animation basic mount on Magisk 1640+

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -3,8 +3,8 @@
 MODDIR=${0%/*};
 
 # restore boot animation for Magisk post-fs mode loading
-if [ ! -f /cache/magisk_mount/system/media/bootanimation.zip ]; then
-  mkdir -p /cache/magisk_mount/system/media;
+if [ ! -f /cache/magisk_mount/system/media/bootanimation.zip -o ! -f /data/adb/magisk_simple/system/media/bootanimation.zip ]; then
+  mkdir -p /cache/magisk_mount/system/media /data/adb/magisk_simple/system/media;
   cp -rf $MODDIR/bootanimation.zip /cache/magisk_mount/system/media/;
+  cp -rf $MODDIR/bootanimation.zip /data/adb/magisk_simple/system/media/;
 fi;
-
