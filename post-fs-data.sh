200c58b0aa9c29231dbd094bfc7d2e194b61e6c6
osm0sis <osm0sis@outlook.com>
2018-05-05 13:30:04 -0300

    use Magisk version code to determine bootanimation basic mount target

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,9 +2,17 @@
 
 MODDIR=${0%/*};
 
-# restore boot animation for Magisk post-fs mode loading
-if [ ! -f /cache/magisk_mount/system/media/bootanimation.zip -o ! -f /data/adb/magisk_simple/system/media/bootanimation.zip ]; then
-  mkdir -p /cache/magisk_mount/system/media /data/adb/magisk_simple/system/media;
-  cp -rf $MODDIR/bootanimation.zip /cache/magisk_mount/system/media/;
-  cp -rf $MODDIR/bootanimation.zip /data/adb/magisk_simple/system/media/;
+# check Magisk version code to find which basic mount method to use
+test -e /data/adb/magisk && adb=adb;
+MAGISK_VER_CODE=$(grep "^MAGISK_VER_CODE=" "/data/$adb/magisk/util_functions.sh" 2>/dev/null | cut -d= -f2);
+if [ "$MAGISK_VER_CODE" -lt 1640 ]; then
+  basicmnt="/cache/magisk_mount";
+else
+  basicmnt="/data/adb/magisk_simple";
+fi;
+
+# restore boot animation for Magisk simple mount mode loading
+if [ ! -f $basicmnt/system/media/bootanimation.zip ]; then
+  mkdir -p $basicmnt/system/media;
+  cp -rf $MODDIR/bootanimation.zip $basicmnt/system/media/;
 fi;
